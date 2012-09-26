6a7941a4799fc0449fef4cff28d28434ee5c1be4
JDuval <jladuval@gmail.com>
2012-09-26 22:19:07 +1200

    added some styling. Unproductive night

--- Home/InitializeDatabase.sql
+++ Home/InitializeDatabase.sql
@@ -1,2 +1,10 @@
 CREATE TRIGGER HappyLinkGenAdj AFTER INSERT On Adjective AS MERGE INTO FluffyLink As HL USING (SELECT Adjective.Text + Copy.Text + Noun.Text FROM Adjective INNER JOIN Adjective AS [Copy] ON Adjective.Text != Copy.Text CROSS JOIN Nouns) AS Source(Text) ON (HL.Text) = (Source.Text) WHEN NOT MATCHED THEN INSERT (Text, LastAccessed) VALUES (Source.Text, NULL);
 CREATE TRIGGER HappyLinkGenNoun AFTER INSERT On Noun AS MERGE INTO FluffyLink As HL USING (SELECT Adjective.Text + Copy.Text +  Noun.Text FROM Adjective INNER JOIN Adjective AS [Copy] ON Adjective.Text != Copy.Text CROSS JOIN Nouns) AS Source(Text) ON (HL.Text) = (Source.Text) WHEN NOT MATCHED THEN INSERT (Text, LastAccessed) VALUES (Source.Text, NULL);
+
+
+SELECT
+CONCAT(a.Text, c.Text, n.Text)
+FROM public."Home_adjective" a
+INNER JOIN public."Home_adjective" c
+ON a.Text != c.Text
+CROSS JOIN public."Home_noun" n;
